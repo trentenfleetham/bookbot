e51611e57e7170c0a04d99a640bb436b8d989293
trentenfleetham <trentenfleetham@gmail.com>
2025-02-07 14:31:56 -0800

    Bookbot that creates report of number of words and alphabetic characters in given book

---
+++ main.py
@@ -0,0 +1,50 @@
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    num_letters = get_num_letters(text)
+    letters_list = character_list(num_letters)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    print()
+
+    for item in letters_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def character_list(character_dictionary):
+    sorted_list = []
+    for char in character_dictionary:
+        sorted_list.append({"char": char, "num": character_dictionary[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+
+def get_num_letters(text):
+    letter_count = {}
+    for char in text:
+        if char.isalpha():
+            char = char.lower()
+            letter_count[char] = letter_count.get(char, 0) + 1
+    return letter_count
+
+
+def get_num_words(text):
+    words = text.split()
+    return len(words)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
