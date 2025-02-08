88f5448ac52b7402b4e8056f181847f8ed8cc3b9
trentenfleetham <trentenfleetham@gmail.com>
2025-02-07 16:03:32 -0800

    added comments for clarity

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def main():
     print(f"{num_words} words found in the document")
     print()
 
+    # prints each character and number count line by line
     for item in letters_list:
         if not item["char"].isalpha():
             continue
@@ -15,11 +16,11 @@ def main():
 
     print("--- End report ---")
 
-
+# key function - tells the sort function how to sort in the 'character_list' function.
 def sort_on(d):
     return d["num"]
 
-
+# converts 'character_dictionary' into a sorted list using 'sort_on' function above as the key and defining the letters as char and the numbers as num.
 def character_list(character_dictionary):
     sorted_list = []
     for char in character_dictionary:
@@ -27,7 +28,7 @@ def character_list(character_dictionary):
     sorted_list.sort(reverse=True, key=sort_on)
     return sorted_list
 
-
+# Finds and returns the count of each letter in a dictionary 'letter_count'
 def get_num_letters(text):
     letter_count = {}
     for char in text:
@@ -36,12 +37,12 @@ def get_num_letters(text):
             letter_count[char] = letter_count.get(char, 0) + 1
     return letter_count
 
-
+# Splits the text into individual words and returns the count (length)
 def get_num_words(text):
     words = text.split()
     return len(words)
 
-
+# Uses defined book path to read the contents into a string 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
